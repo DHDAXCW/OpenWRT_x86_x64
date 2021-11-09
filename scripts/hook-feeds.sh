db0bfc931702c6000c2c65bbf56bd4dcee704c62
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2021-11-09 16:51:52 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -13,10 +13,10 @@ svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-gowebdav 
 svn co https://github.com/immortalwrt/packages/trunk/net/gowebdav packages/net/gowebdav
 
 # Add luci-app-netdata
-rm -rf packages/admin/netdata
-svn co https://github.com/281677160/openwrt-package/trunk/feeds/packages/net/netdata packages/admin/netdata
-rm -rf ../package/lean/luci-app-netdata
-svn co https://github.com/281677160/openwrt-package/trunk/feeds/luci/applications/luci-app-netdata luci/applications/luci-app-netdata
+# rm -rf packages/admin/netdata
+# svn co https://github.com/281677160/openwrt-package/trunk/feeds/packages/net/netdata packages/admin/netdata
+# rm -rf ../package/lean/luci-app-netdata
+# svn co https://github.com/281677160/openwrt-package/trunk/feeds/luci/applications/luci-app-netdata luci/applications/luci-app-netdata
 
 # Add tmate
 git clone --depth=1 https://github.com/immortalwrt/openwrt-tmate
