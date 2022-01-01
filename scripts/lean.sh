2ab7d3a8354dbaa7d3d648552d23f043d0130212
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-01-01 15:14:35 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -183,8 +183,9 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='FusionWrt'' package/lean/default-settings/files/zzz-default-settings
 sed -i "s/OpenWrt /DHDAXCW @ FusionWrt $(TZ=UTC-8 date "+%Y%m%d") /g" package/lean/default-settings/files/zzz-default-settings
 # find package/*/ feeds/*/ -maxdepth 6 -path "*luci-app-smartdns/luasrc/controller/smartdns.lua" | xargs -i sed -i 's/\"SmartDNS\")\, 4/\"SmartDNS\")\, 3/g' {} 
+
 # Test kernel 5.10
-sed -i 's/5.10/5.15/g' target/linux/x86/Makefile
+#sed -i 's/5.10/5.15/g' target/linux/x86/Makefile
 
 # Custom configs
 # git am $GITHUB_WORKSPACE/patches/lean/*.patch
