f7abb42e01ba4cf389c032cb7b71e7728353230d
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-01-08 20:47:15 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -2,6 +2,9 @@
 rm -rf ./feeds/packages/utils/runc/Makefile
 svn export https://github.com/openwrt/packages/trunk/utils/runc/Makefile ./feeds/packages/utils/runc/Makefile
 
+rm -rf target/linux
+svn co https://github.com/DHDAXCW/RK356X/trunk/target/linux
+
 # fix netdata
 rm -rf ./feeds/packages/admin/netdata
 svn co https://github.com/DHDAXCW/packages/branches/ok/admin/netdata ./feeds/packages/admin/netdata
