6ccfeae33ba56756aef914b97254617b5e4190a2
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-02-16 11:42:20 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -37,9 +37,9 @@ popd
 pushd customfeeds/luci
 export luci_feed="$(pwd)"
 popd
-sed -i '/src-git packages/d' feeds.conf.default
+sed -i '/src-git-full packages/d' feeds.conf.default
 echo "src-link packages $packages_feed" >> feeds.conf.default
-sed -i '/src-git luci/d' feeds.conf.default
+sed -i '/src-git-full luci/d' feeds.conf.default
 echo "src-link luci $luci_feed" >> feeds.conf.default
 
 # Update feeds
