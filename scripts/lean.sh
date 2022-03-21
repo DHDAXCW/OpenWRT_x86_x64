4ebc6ed3aae1afac9b83a1bf58d64c3e30acaa83
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-03-21 14:56:33 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -166,7 +166,7 @@ popd
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='FusionWrt'' package/lean/default-settings/files/zzz-default-settings
 sed -i "s/OpenWrt /DHDAXCW @ FusionWrt /g" package/lean/default-settings/files/zzz-default-settings
 
