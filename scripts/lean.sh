b1df5a21fc3795aabcf6615394aa421a2d5a56ae
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-04-25 08:41:06 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -172,7 +172,3 @@ sed -i "s/OpenWrt /DHDAXCW @ FusionWrt /g" package/lean/default-settings/files/z
 # sed -i 's/5.4/5.10/g' target/linux/x86/Makefile
 
 echo -e " DHDAXCW's FusionWrt built on "$(date +%Y.%m.%d)"\n -----------------------------------------------------" >> package/base-files/files/etc/banner
-echo 'net.bridge.bridge-nf-call-iptables=0' >> package/base-files/files/etc/sysctl.conf
-echo 'net.bridge.bridge-nf-call-ip6tables=0' >> package/base-files/files/etc/sysctl.conf
-echo 'net.bridge.bridge-nf-call-arptables=0' >> package/base-files/files/etc/sysctl.conf
-echo 'net.bridge.bridge-nf-filter-vlan-tagged=0' >> package/base-files/files/etc/sysctl.conf
