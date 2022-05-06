097d46f3ecf868ac1a675802d95ecbccf8831b28
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-05-06 09:07:42 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -4,7 +4,8 @@ uci set luci.main.mediaurlbase='/luci-static/argon'
 
 # Disable opkg signature check
 # sed -i 's/option check_signature/# option check_signature/g' /etc/opkg.conf
-
+# 禁用ipv6前缀
+sed -i 's/^[^#].*option ula/#&/' /etc/config/network
 # Disable autostart by default for some packages
 cd /etc/rc.d
 rm -f S98udptools || true
