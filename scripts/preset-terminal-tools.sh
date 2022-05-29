40293fdc7c52bc1caffd58a9181520a26ab528be
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-05-29 09:50:57 +0800

    Update preset-terminal-tools.sh

--- scripts/preset-terminal-tools.sh
+++ scripts/preset-terminal-tools.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#=================================================
+# File name: preset-terminal-tools.sh
+# System Required: Linux
+# Version: 1.0
+# Lisence: MIT
+# Author: SuLingGG
+# Blog: https://mlapp.cn
+#=================================================
 mkdir -p files/root
 pushd files/root
 
