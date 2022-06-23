32901136ac990074fad10a3d2280a762953c9223
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-06-23 08:59:12 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -39,13 +39,13 @@ git clone --depth=1 https://github.com/ysc3839/luci-proto-minieap
 git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
 
 # Add luci-app-adguardhome
-svn co https://github.com/Lienol/openwrt-package/branches/other/luci-app-adguardhome
+svn co https://github.com/Boos4721/OpenWrt-Packages/trunk/luci-app-adguardhome
 
 # Add ddnsto & linkease
-svn co https://github.com/DHDAXCW/nas-packages-luci/trunk/luci/luci-app-ddnsto
-svn co https://github.com/DHDAXCW/nas-packages-luci/trunk/luci/luci-app-linkease
-svn co https://github.com/DHDAXCW/nas-packages/trunk/network/services/ddnsto
-svn co https://github.com/DHDAXCW/nas-packages/trunk/network/services/linkease
+svn co https://github.com/linkease/nas-packages-luci/trunk/luci/luci-app-ddnsto
+svn co https://github.com/linkease/nas-packages-luci/trunk/luci/luci-app-linkease
+svn co https://github.com/linkease/nas-packages/trunk/network/services/ddnsto
+svn co https://github.com/linkease/nas-packages/trunk/network/services/linkease
 
 # Add OpenClash
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
