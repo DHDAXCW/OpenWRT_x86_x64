b398315cc9168052f4185b071b4cf12ff59101bd
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-10-24 23:59:20 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -8,7 +8,7 @@
 # Blog: https://mlapp.cn
 #=================================================
 # Set default theme to luci-theme-argon
-uci set luci.main.mediaurlbase='/luci-static/argon'
+uci set luci.main.mediaurlbase='/luci-static/argonne'
 
 # Disable opkg signature check
 # sed -i 's/option check_signature/# option check_signature/g' /etc/opkg.conf
