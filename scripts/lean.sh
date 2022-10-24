5884747372ae36300c70d101089839a901ed0c38
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-10-24 21:08:43 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -115,5 +115,5 @@ sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 
-# Test kernel 5.19
-# sed -i 's/5.15/6.0/g' target/linux/x86/Makefile
+# Test kernel 6.0
+sed -i 's/5.15/6.0/g' target/linux/x86/Makefile
