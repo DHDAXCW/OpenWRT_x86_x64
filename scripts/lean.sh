c8d250846483a74cca261998d49c3a7a627e0873
骷髅头 <lasstp5011@gmail.com>
2023-03-09 16:13:46 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -9,6 +9,13 @@
 #=================================================
 # Clone community packages to package/community
 
+rm -rf package/kernel/mac80211
+rm -rf package/kernel/mt76
+rm -rf package/network/services/hostapd
+svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mac80211 package/kernel/mac80211
+svn export https://github.com/DHDAXCW/LubanCat-LEDE/trunk/scripts/mt76 package/kernel/mt76
+svn export https://github.com/openwrt/openwrt/trunk/package/network/services/hostapd package/network/services/hostapd
+
 # alist
 git clone https://github.com/sbwml/luci-app-alist package/alist
 rm -rf feeds/packages/lang/golang
