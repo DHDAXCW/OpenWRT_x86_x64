c9c7a5f22fb3bd01ded86e49e54c6bf2700502d2
骷髅头 <lasstp5011@gmail.com>
2023-03-11 17:01:08 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -135,4 +135,4 @@ cp -f $GITHUB_WORKSPACE/02_network target/linux/x86/base-files/etc/board.d/02_ne
 sed -i 's/5.15/6.1/g' target/linux/x86/Makefile
 rm -rf package/base-files/files/etc/banner
 wget -P package/base-files/files/etc https://raw.githubusercontent.com/DHDAXCW/lede-rockchip/stable/package/base-files/files/etc/banner
-cp -r ../target/linux/generic/pending-6.1/ ./target/linux/generic/pending-6.1/
+cp -r ../target/linux/generic/pending-6.1/ ./target/linux/generic/
