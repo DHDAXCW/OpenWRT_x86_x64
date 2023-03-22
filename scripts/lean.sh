23dce2500ae4491fed80089a14edd9722f8259b2
骷髅头 <lasstp5011@gmail.com>
2023-03-22 09:28:26 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -35,9 +35,6 @@ git clone --depth=1 https://github.com/fw876/helloworld
 
 # Add luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall
-cd openwrt-passwall
-git checkout 4fd4bf8
-cd ../
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2
 svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
 
