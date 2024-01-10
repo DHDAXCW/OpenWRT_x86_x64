c44a19c2d45b843fbd83d3fea5c45d1a22c789a5
骷髅头 <lasstp5011@gmail.com>
2024-01-10 16:43:59 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -76,6 +76,7 @@ git clone --depth=1 https://github.com/destan19/OpenAppFilter
 rm -rf ../../customfeeds/luci/applications/luci-app-aliyundrive-webdav
 rm -rf ../../customfeeds/packages/multimedia/aliyundrive-webdav
 git clone --depth=1 https://github.com/messense/aliyundrive-webdav
+mkdir -p linkease
 popd
  
 # Mod zzz-default-settings
@@ -85,7 +86,6 @@ sed -i '/18.06/d' zzz-default-settings
 export orig_version=$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
 export date_version=$(date -d "$(rdate -n -4 -p ntp.aliyun.com)" +'%Y-%m-%d')
 sed -i "s/${orig_version}/${orig_version} (${date_version})/g" zzz-default-settings
-mkdir -p linkease
 popd
 
 # Add ddnsto & linkease
