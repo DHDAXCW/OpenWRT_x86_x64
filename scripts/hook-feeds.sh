c1fbb77caab9a5004407dd28bb924044c2dc031d
骷髅头 <lasstp5011@gmail.com>
2024-01-10 14:40:59 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -1,30 +1,4 @@
 #!/bin/bash
-# Svn checkout packages from immortalwrt's repository
-pushd customfeeds
-
-# Add luci-app-eqos
-svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-eqos luci/applications/luci-app-eqos
-
-# Add luci-proto-modemmanager
-svn co https://github.com/immortalwrt/luci/trunk/protocols/luci-proto-modemmanager luci/protocols/luci-proto-modemmanager
-
-# Add luci-app-gowebdav
-svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-gowebdav luci/applications/luci-app-gowebdav
-rm -rf packages/net/gowebdav
-svn co https://github.com/immortalwrt/packages/trunk/net/gowebdav packages/net/gowebdav
-
-# Add tmate
-git clone --depth=1 https://github.com/immortalwrt/openwrt-tmate
-
-# Add gotop
-rm -rf packages/admin/gotop
-svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/admin/gotop packages/admin/gotop
-
-# Add minieap
-rm -rf packages/net/minieap
-svn co https://github.com/immortalwrt/packages/trunk/net/minieap packages/net/minieap
-popd
-
 # Set to local feeds
 pushd customfeeds/packages
 export packages_feed="$(pwd)"
