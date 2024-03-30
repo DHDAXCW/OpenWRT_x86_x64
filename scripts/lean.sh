1f725b92477836a265d7621fe4b6dbeb8e291b36
骷髅头 <lasstp5011@gmail.com>
2024-03-30 09:19:07 +0800

    fix
    https://github.com/sbwml/luci-app-alist/issues/101

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -10,7 +10,7 @@
 # Clone community packages to package/community
 
 # alist
-git clone https://github.com/DHDAXCW/luci-app-alist package/alist
+git clone https://github.com/sbwml/luci-app-alist package/alist
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 
