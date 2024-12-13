3701630dfdf85e24047a016343d03b31abddea41
骷髅头 <lasstp5011@gmail.com>
2024-12-13 20:10:36 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -18,6 +18,12 @@ rm -rf ../../customfeeds/luci/applications/luci-app-kodexplorer
 rm -rf openwrt-package/verysync
 rm -rf openwrt-package/luci-app-verysync
 
+# Add Lienol's Packages
+git clone --depth=1 https://github.com/Lienol/openwrt-package
+rm -rf ../../customfeeds/luci/applications/luci-app-kodexplorer
+rm -rf openwrt-package/verysync
+rm -rf openwrt-package/luci-app-verysync
+
 # apppppppp
 git clone --depth=1 https://github.com/DHDAXCW/dhdaxcw-app
 
