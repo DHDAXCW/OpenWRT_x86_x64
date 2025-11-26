33dfade9167f12bfce987d24482d68e0651626a6
骷髅头 <lasstp5011@gmail.com>
2025-11-26 19:51:33 +0800

    Update diy-part1.sh

--- immortalwrt/diy-part1.sh
+++ immortalwrt/diy-part1.sh
@@ -27,8 +27,6 @@ git clone --depth=1 https://github.com/linkease/istore
 popd
 
 # add luci-app-mosdns
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
