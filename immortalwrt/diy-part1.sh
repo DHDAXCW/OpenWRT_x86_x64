4c64dfc95b83f5bab446d1b77b4ff01c1edb07e0
骷髅头 <lasstp5011@gmail.com>
2025-12-13 10:00:33 +0800

    Update golang package branch from 23.x to 24.x

--- immortalwrt/diy-part1.sh
+++ immortalwrt/diy-part1.sh
@@ -30,7 +30,7 @@ popd
 
 # add luci-app-mosdns
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
